54c23249b930873f101cd84e2001f537c7b3eba1
tamasstephen <kicsitnagyon@gmail.com>
2023-06-04 14:03:36 +0200

    feat: Add multer middleware

--- backend/src/middlewares/video.middleware.ts
+++ backend/src/middlewares/video.middleware.ts
@@ -6,10 +6,13 @@ const videoStorage = multer.diskStorage({
     cb(null, "./video");
   },
   filename: (req, file, cb) => {
+    const videoDir = path.resolve(__dirname, "../../video");
     const fileName = `${Date.now()}${path.extname(file.originalname)}`;
     try {
       cb(null, fileName);
-      req.body.filePath = `http://localhost:4000/video/${fileName}`;
+      req.body.path = `${videoDir}/${fileName}`;
+      req.body.fileName = fileName;
+      req.body.originalFileName = file.originalname;
     } catch (err) {
       console.log(err);
       req.body.error = err;
