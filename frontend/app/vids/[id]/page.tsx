360a2568d7b8cdaa3041bca0bca328dedb55226f
tamasstephen <kicsitnagyon@gmail.com>
2023-06-05 14:37:05 +0200

    feat: Get video details

--- frontend/app/vids/[id]/page.tsx
+++ frontend/app/vids/[id]/page.tsx
@@ -1,16 +1,33 @@
 "use client";
 
 import { usePathname } from "next/navigation";
+import { useQuery } from "react-query";
 import Video from "./component/video";
+import VideoDetails from "./component/VideoDetails";
+import Div from "@/app/components/Div";
 
 export default function SingleVideoPage() {
   const pathName = usePathname();
   const videoId = pathName.split("/")[2];
+  const { data, status, isLoading, isError, error } = useQuery(
+    `video-${videoId}`,
+    () =>
+      fetch(`http://localhost:3001/api/video/${videoId}/data`).then((res) =>
+        res.json()
+      )
+  );
   return (
-    <div>
-      <h1>Single Video Page</h1>
-      <p>id: {videoId}</p>
-      <Video videoId={videoId} />
+    <div className="mt-8">
+      <Div width="large">
+        <Video videoId={videoId} />
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : isError ? (
+          <p>Error: {error}</p>
+        ) : (
+          <VideoDetails title={data.title} description={data.description} />
+        )}
+      </Div>
     </div>
   );
 }
