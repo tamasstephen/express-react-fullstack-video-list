1440e1dd08d828f0588aaad412c320c7e5e12999
tamasstephen <kicsitnagyon@gmail.com>
2023-06-05 14:34:27 +0200

    feat: Add sizes to wrapper element

--- frontend/app/components/Div.tsx
+++ frontend/app/components/Div.tsx
@@ -1,7 +1,24 @@
-export default function Div({ children }: { children: React.ReactNode }) {
+interface DivProps {
+  children: React.ReactNode;
+  width?: "small" | "medium" | "large";
+}
+
+const widthMap = {
+  small: "w-72",
+  medium: "w-[90%] lg:w-1/2 max-w-5xl",
+  large: "w-[90%] lg:w-9/12 max-w-7xl",
+};
+
+export default function Div({ children, width }: DivProps) {
   return (
-    <div className="flex flex-col items-center justify-center w-72">
-      {children}
+    <div className="flex justify-center">
+      <div
+        className={`flex flex-col items-center justify-center ${
+          width ? widthMap[width] : widthMap.small
+        }`}
+      >
+        {children}
+      </div>
     </div>
   );
 }
