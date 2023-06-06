04dcd83901a352c74de0041d166e042e66a3a835
tamasstephen <kicsitnagyon@gmail.com>
2023-06-06 08:09:28 +0200

    refactor: Extract like into the Button copmonent

--- frontend/app/components/video/Like/LikeView.tsx
+++ frontend/app/components/video/Like/LikeView.tsx
@@ -3,16 +3,20 @@ interface LikeViewProps {
 }
 
 import Image from "next/image";
+import Button from "../../Button";
 
 const LikeView = ({ wasLikedByUser }: LikeViewProps) => {
   const activeStyle = wasLikedByUser ? "text-red-500" : "text-gray-700";
   return (
-    <div
+    /*   <div
       className={`flex rounded-md border-2 border-gray-300 px-2 py-1 ${activeStyle}`}
-    >
+    > */
+    <Button buttonType="secondary">
       <Image alt="like" src="/heart-solid.svg" width={16} height={16} />
-      <p className="ml-2 font-medium">Like</p>
-    </div>
+      <p className="ml-2 font-medium text-primary-text">Like</p>
+    </Button>
+    /*     </div>
+     */
   );
 };
 
